98f9e862e0584b1d6fdb1f90363ea70055067b08
Bianca Pedrosa <bianca.ifsp@gmail.com>
2022-05-25 13:18:57 -0300

    Update index.js

--- firestore2022/public/src/index.js
+++ firestore2022/public/src/index.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
 const app= initializeApp(firebaseConfig);
 const db=getFirestore();
 
-//const db=getDatabase();
-
 /* -------- References ---------- */
 let varId=document.getElementById("formId");
 let varTitulo=document.getElementById("formTitulo");
